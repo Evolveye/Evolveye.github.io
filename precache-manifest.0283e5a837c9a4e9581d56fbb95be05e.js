8128e391df8a4b347803a5f16e61593069f1ab2f
Evolveye <stolarski.paw@gmail.com>
2020-06-10 01:44:43 +0200

    Updates

---
+++ precache-manifest.0283e5a837c9a4e9581d56fbb95be05e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c78deaaf11d218dbd816c9e286b08ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aefce739c731cbb43ad",
+    "url": "/static/css/main.257ecac0.chunk.css"
+  },
+  {
+    "revision": "8c41d7ed5f5772ede170",
+    "url": "/static/js/2.3f6f6d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f6f6d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aefce739c731cbb43ad",
+    "url": "/static/js/main.02a04f57.chunk.js"
+  },
+  {
+    "revision": "4a58f07abc3c608b88a0",
+    "url": "/static/js/runtime-main.118059e6.js"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/static/media/javascript.0e4fdce8.png"
+  },
+  {
+    "revision": "e8c4671b1cca100eed3c9cfed1f28b05",
+    "url": "/static/media/kotlin.e8c4671b.png"
+  },
+  {
+    "revision": "647bb273ed04057e49c9becb5d4aaac5",
+    "url": "/static/media/mail.647bb273.png"
+  },
+  {
+    "revision": "4ffafe9b4dbb46b2a4ed80abfc777aa4",
+    "url": "/static/media/node.js.4ffafe9b.png"
+  },
+  {
+    "revision": "7bb33c1694186552ec0464e23fc09bbe",
+    "url": "/static/media/rust.7bb33c16.png"
+  }
+]);
