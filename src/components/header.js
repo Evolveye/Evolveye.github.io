347c0c28674f0af609228e77e3afaf508f2757fb
Evolveye <stolarski.paw@gmail.com>
2020-08-29 19:06:30 +0200

    Checkbox for theme changing

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,25 @@ import Avatar from "./avatar"
 
 import styles from "./header.module.css"
 
-export default () => <header className={styles.header}>
-  <Link to="/" className="is-noDecorative"><Avatar className={styles.avatar}/></Link>
-  <nav className={styles.nav}>
-    <Link to="/" className={styles.link}>Dostępność (accessibility)</Link>
-    <Link to="/" className={styles.link}>Polityka prywatności</Link>
-    <Link to="/" className={styles.link}>O blogu</Link>
-    <Link to="/" className={styles.link}>O autorze</Link>
-  </nav>
-  <aside className={styles.empty} />
-</header>
+
+export default ({ themeChanger }) => {
+  const theme = localStorage.getItem( `theme` )
+
+  return <header className={styles.header}>
+    <h1 className={styles.title}>
+      <Link to="/" className="is-noDecorative"><Avatar className={styles.avatar} /></Link>
+    </h1>
+    <nav className={styles.nav}>
+      <Link to="/" className={styles.link}>Dostępność (accessibility)</Link>
+      <Link to="/" className={styles.link}>Polityka prywatności</Link>
+      <Link to="/" className={styles.link}>O blogu</Link>
+      <Link to="/" className={styles.link}>O autorze</Link>
+    </nav>
+    <aside className={styles.aside}>
+      <label>
+        <input type="checkbox" checked={theme === `dark`} onChange={e => themeChanger( e.target.checked )} />
+        Ciemny motyw
+      </label>
+    </aside>
+  </header>
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,9 +7,22 @@ import Footer from "./footer"
 import "./sanitize.css"
 import styles from "./layout.module.css"
 
-export default ({ className=``, children }) => <div className={styles.layout}>
-  <SEO title="Home" />
-  <Header />
-  <main className={`${styles.main} ${className}`}>{children}</main>
-  <Footer />
-</div>
+export default class Layout extends React.Component {
+  state = {
+    theme: localStorage.getItem( `theme` ) === `dark` ? styles.isDark : ``,
+  }
+
+  changeTheme = isDark => {
+    const theme = isDark ? `dark` : ``
+    this.setState( { theme:(theme === `dark` ? styles.isDark : ``) } )
+
+    localStorage.setItem( `theme`, theme )
+  }
+
+  render = () => <div className={`${styles.layout} ${this.state.theme}`}>
+    <SEO title="Home" />
+    <Header themeChanger={this.changeTheme}/>
+    <main className={`${styles.main} ${this.props.className}`}>{this.props.children}</main>
+    <Footer />
+  </div>
+}
