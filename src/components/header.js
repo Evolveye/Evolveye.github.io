459ba4fb48218c12a93bc0d2bc332d1ad66dc07d
Evolveye <stolarski.paw@gmail.com>
2020-08-29 15:12:14 +0200

    Non-functional nav in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Avatar from "./avatar"
 
 import styles from "./header.module.css"
 
 export default () => <header className={styles.header}>
-  <Avatar className={styles.avatar}/>
+  <Link to="/" className="is-noDecorative"><Avatar className={styles.avatar}/></Link>
+  <nav className={styles.nav}>
+    <Link to="/" className={styles.link}>Dostępność (accessibility)</Link>
+    <Link to="/" className={styles.link}>Polityka prywatności</Link>
+    <Link to="/" className={styles.link}>O blogu</Link>
+    <Link to="/" className={styles.link}>O autorze</Link>
+  </nav>
+  <aside className={styles.empty} />
 </header>
