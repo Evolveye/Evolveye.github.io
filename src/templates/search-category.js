56385433e5a13ca5580f1c79b16d21857aea82e3
Evolveye <stolarski.paw@gmail.com>
2020-09-24 20:05:30 +0200

    Search templates restyle

--- src/templates/search-category.js
+++ src/templates/search-category.js
@@ -2,31 +2,31 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Post from "../components/post"
+import { BlogpostEntry } from "../components/post"
 
 import styles from "./search.module.css"
 
-export default ({ data, pageContext }) => <Layout className={styles.searchPage}>
-  <section className={styles.info}>
-    <h2 className={styles.title}>O wynikach</h2>
-    <ul>
-      <li>Przeszukiwana kategoria: <Link to={`/category/${pageContext.category}`}>{pageContext.category}</Link></li>
-      <li>Ilość wyników: {data.allMdx.nodes.length}</li>
-    </ul>
-  </section>
-  <section className={styles.posts}>
-    {
-      data.allMdx.nodes.map( ({ id, excerpt, frontmatter, fields }) => <Post
-        key={id}
-        titleLinkAddress={`/post${fields.slug}`}
-        frontmatter={frontmatter}
-        body={excerpt}
-        isEntry={true}
-      /> )
-    }
-  </section>
-  <aside className={styles.empty} />
-</Layout>
+export default ({ data, pageContext }) =>
+  <Layout className={styles.searchPage}>
+    <aside className={styles.empty} />
+    <section className={styles.posts}>
+      {
+        data.allMdx.nodes.map( ({ id, excerpt, frontmatter, fields }) => <BlogpostEntry
+          key={id}
+          titleLinkAddress={`/post${fields.slug}`}
+          frontmatter={frontmatter}
+          body={excerpt}
+        /> )
+      }
+    </section>
+    <section className={styles.info}>
+      <h2 className={styles.title}>O wynikach</h2>
+      <ul>
+        <li>Przeszukiwana kategoria: <Link to={`/category/${pageContext.category}`}>{pageContext.category}</Link></li>
+        <li>Ilość wyników: {data.allMdx.nodes.length}</li>
+      </ul>
+    </section>
+  </Layout>
 
 export const query = graphql`
   query PostsByCategory( $category:String! ) {
