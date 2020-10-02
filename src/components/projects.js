d4fb0ad3fd3f33f44ef2b07eae20f67b41c27eab
Evolveye <stolarski.paw@gmail.com>
2020-10-03 01:47:14 +0200

    Projects section translation

--- src/components/projects.js
+++ src/components/projects.js
@@ -55,25 +55,32 @@ const projectsData = [
   {
     title: {
       pl: `Cactu`,
-      en: `English`,
+      en: `Cactu`,
     },
     fluidName: `cactu`,
     description: {
       pl: <>
-        Rzeczy które tworzę lubię naznaczać konkretnym logiem - <a href="https://codecactu.github.io/">marką Cactu</a>.
+        Rzeczy które tworzę lubię naznaczać konkretnym logiem -- <a href="https://codecactu.github.io/">marką Cactu</a>.
         Być moze kiedyś ktoś będzie chciał tworzyć razem ze mną pod wspólnym znakiem,
-        więc sygnowanie projektów znakiem towarowym zamiast nazwiskiem uważam za stosowne.
+        więc sygnowanie projektów znakiem towarowym połączonym niejako z mym nazwiskiem uważam za stosowne.
         Dodatkowo logo zostało przygotowane tak, aby było łatwe do odwzorowania za pomocą grafiki wektorowej
         (bowiem zostało za pomocą niej przygotowane). Sprawia to, że naznaczanie nim tworów
         (przykłądowo porpzez wstawienie do stopki) jest proste, w przeciwieństwie do mojego, niewektorowego, avatara.
       </>,
-      en: <>English</>,
+      en: <>
+        I like to mark a things I create with specific logo -- <a href="https://codecactu.github.io/">the Cactu brand</a>.
+        Perhaps someday somebody will want to make something with me under common sign,
+        so I consider signing the projects with a trademark connected with my name appropriate.
+        In addition, the logo has been prepared so that it is easy to reproduce with vector graphics
+        (it was made in that graphic). It makes marking creations with it (for example, by inserting into footer)
+        very easy, unlike to my non-vector eye-like avatar.
+      </>,
     }
   },
   {
     title: {
-      pl:`Bot discordowy`,
-      en: `English`,
+      pl:`Discordowy bot`,
+      en: `Discord bot`,
     },
     fluidName: `discordBot`,
     description: {
@@ -83,13 +90,18 @@ const projectsData = [
         Obecnie z pomocą tego bota automatyzuję wszystko co potrzebuję. Czy to mało ambitne odpowiedzi tekstem na tekst,
         czy to edycja grafiki lub odpytywanie zewnętrznych API.
       </>,
-      en: <>English</>,
+      en: <>
+        All have been started from starting small community. Over time I became interested in the API provided by
+        Discord platform. This is how the <a href="https://discord.js.org/#/">discord.js</a> based framework was created.
+        Currently with the help of that bot I automate everything I need. From unambitious text-to-text responses,
+        thought graphic editing, to querying external APIs.
+      </>,
     }
   },
   {
     title: {
       pl: `Silnik grafiki trójwymiarowej`,
-      en: `English`,
+      en: `3D graphgic engine`,
     },
     fluidName: `webGl`,
     description: {
@@ -97,29 +109,41 @@ const projectsData = [
         Nie lubię korzystać z bibliotek czy frameworków jeśli nie wiem co kryje się w ich wnętrzu.
         Co sprawia, że dzieje się to co się dzieje.
         Jako, że chciałem nauczyć się biblioteki <a href="https://threejs.org/">Three.js</a> przeznaczonej
-        do tworzenia rysowania elementów trójwymairowych, to nie dawał mi spokoju brak umiejętnosci samodzielnego rysowania w 3D.
+        do rysowania rzeczy trójwymairowych, to nie dawał mi spokoju brak umiejętnosci samodzielnego rysowania w 3D.
         Postanowiłem więc w ramach rozgrzewki zrobić swój prymitywny silnik do wyświetlania kształtów 3D.
         Podobnie mam z innymi technologiami -- jeśli nie wiem jak działa, to mam wewnętrzną potrzebę się dowiedzieć.
       </>,
-      en: <>English</>,
+      en: <>
+        I don't like to use libraries or framework if I don't know what is inside.
+        What makes what is happening. As i wanted to learn <a href="https://threejs.org/">Three.js</a> library for
+        drawing three-dimensional things, the lack of the ability to draw in 3D by myself bothered me.
+        So, I decided to make my own primitive 3D engine as part of my warm-up.
+        The same to others technologies -- if I don't know how it works, I have inner need to find out.
+      </>,
     }
   },
   {
     title: {
       pl: `Ta strona`,
-      en: `English`,
+      en: `This website`,
     },
     fluidName: `avatar`,
     description: {
       pl: <>
         Programuję w zasadzie codziennie. Pomysłów mam wiele, chęci nauki jeszcze wiecej.
         Wiele małych dzieł przepadło z tej racji że nie miałem kiedys konta github,
-        lub nie widziałem sensu we wrzucaniu nań projektów. Doszło też w końcu do tego,
+        lub nie widziałem sensu we wrzucaniu nań projektów. W końcu do tego,
         że postanowiłem zrobić sobie stronę z której byłbym zadowolony, a na której mógłbym
         prowadzić swojego devloga. Więcej treści na temat strony to niczym treść o tutejszym blogu,
-        o którym wiecej napisałem <a href="/">pod tym linkiem</a>
+        o którym wiecej napisałem <a href="/">pod tym linkiem</a>.
       </>,
-      en: <>English</>,
+      en: <>
+        I program basically everyday. I have many ideas, the willingness to learn even more.
+        Many small works have been lost because I didn't have Github account, or saw no point
+        to upload projects to it. Finally, I decided to make a website that I would be happy about,
+        and where I could run my devlog (polish only, sorry; but maybe in the future...).
+        If you know polish, you can look <a href="/">here</a>.
+      </>,
     }
   },
 ]
@@ -129,7 +153,9 @@ export default ({ langKey }) => {
   const queryData = useStaticQuery( query )
 
   return <section>
-    <h2 className={`h2 boxed-title is-red`}>{langKey === `pl` ? `Wyróżniona aktywność` : `English`}</h2>
+    <h2 className={`h2 boxed-title is-red`}>
+      {langKey === `pl` ? `Projekty coś o mnie mówiące` : `Project that say something about me`}
+    </h2>
 
     <div className={styles.projectsWrapper}>
       {
