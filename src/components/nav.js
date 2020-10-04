b952b160bf2f02193a9cfa1d35f631dd717fd316
Evolveye <stolarski.paw@gmail.com>
2020-10-04 20:35:04 +0200

    Language switcher

--- src/components/nav.js
+++ src/components/nav.js
@@ -33,6 +33,20 @@ const query = graphql`
         }
       }
     }
+    pl: file( relativePath:{ eq:"flag_pl.png" } ) {
+      childImageSharp {
+        fluid( maxWidth:100 ) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    en: file( relativePath:{ eq:"flag_en.png" } ) {
+      childImageSharp {
+        fluid( maxWidth:100 ) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
   }
 `
 
@@ -50,9 +64,28 @@ const projectsData = [
       pl: `Blog`,
       en: `Blog (polish only)`,
     },
+    onlyLangKey: `pl`,
     fluidName: `quote`,
     address: `/blog/`,
-    polishOnly: true,
+  },
+]
+
+const languages = [
+  {
+    title: {
+      en: `Homepage (polish)`,
+    },
+    onlyLangKey: `en`,
+    fluidName: `pl`,
+    lKey: `pl`
+  },
+  {
+    title: {
+      pl: `Strona domowa (angielska)`,
+    },
+    onlyLangKey: `pl`,
+    fluidName: `en`,
+    lKey: `en`
   },
 ]
 
@@ -63,14 +96,14 @@ export default ({ langKey, className }) => {
   return <nav className={`${className} ${styles.nav}`}>
     <ul className={styles.itemList}>
       {
-        projectsData.map( ({ title, fluidName, address, polishOnly }) => polishOnly && langKey !== `pl` ? null : (
-          <li key={title[ langKey ]} className={styles.item}>
-            <Link langKey={langKey} to={address} className={`neumorphizm-white is-hoverable is-not-decorative ${styles.link}`}>
-              <Img fluid={queryData[ fluidName ].childImageSharp.fluid} alt={`${title[ langKey ]} icon`} />
+        [ ...projectsData, ...languages ].map( ({ title, fluidName, address=`/`, lKey=langKey, onlyLangKey }) => onlyLangKey && onlyLangKey !== langKey ? null :
+          <li key={fluidName} className={styles.item}>
+            <Link langKey={lKey} className={`neumorphizm-white is-hoverable is-not-decorative ${styles.link}`} to={address}>
+              <Img fluid={queryData[ fluidName ].childImageSharp.fluid} alt={`${title[ langKey ]} icon`} className={styles.icon} />
             </Link>
             <div className={styles.title}>{title[ langKey ]}</div>
           </li>
-        ) )
+        )
       }
     </ul>
   </nav>
